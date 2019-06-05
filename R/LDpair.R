3ec519954647eab066ca5cf0de83c094eaae8c20
machiela <mitchjunk@gmail.com>
2019-06-05 15:38:24 -0400

    Add httr::

--- R/LDpair.R
+++ R/LDpair.R
@@ -132,7 +132,7 @@ url_str <- paste(url, "?", paste(unlist(body), collapse = "&"), sep="")
 
 # GET command, request to the web server
 raw_out <- httr::GET(url=url_str)
-stop_for_status(raw_out)
+httr::stop_for_status(raw_out)
 # Parse response object from web server
 data_out <- read.delim(textConnection(content(raw_out, "text", encoding = "UTF-8")), header=T, as.is = T, sep="\t")
 
