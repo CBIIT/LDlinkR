7ed224385250b8631a81513be0f7ae8ba7ac4691
Tim <timothy.a.myers@gmail.com>
2020-12-15 08:17:02 -0500

    add testthat tests for LDexpress

---
+++ tests/testthat/test-ldexpress.R
@@ -0,0 +1,43 @@
+context("test-ldexpress")
+
+test_that("ldexpress throws an error for invalid input arguments", {
+  skip_on_cran()
+  expect_error(LDexpress(snps = "rs0", pop = "CEU", tissue =  c("ADI_SUB", "ADI_VIS_OME"),
+                         token = Sys.getenv("LDLINK_TOKEN")
+                        )
+               )
+  expect_error(LDexpress(snps = "rs4", pop = "CE", tissue =  c("ADI_SUB", "ADI_VIS_OME"),
+                         token = Sys.getenv("LDLINK_TOKEN")
+                         )
+              )
+  expect_error(LDexpress(snps = "rs0", r2d = "r", tissue =  c("ADI_SUB", "ADI_VIS_OME"),
+                         token = Sys.getenv("LDLINK_TOKEN")
+                         )
+               )
+  expect_error(LDexpress(snps = "rs4", tissue = "ADI_xxx",
+                         token = Sys.getenv("LDLINK_TOKEN")
+                         )
+              )
+  expect_error(LDexpress(snps = "rs4", tissue = c("Adipose_Subcutaneous", "ADI_VIS_OME"),
+                         token = Sys.getenv("LDLINK_TOKEN")
+                         )
+               )
+
+})
+
+test_that("ldexpress throws an error when thresholds are outside acceptable range", {
+  skip_on_cran()
+  expect_error(LDexpress(snps = "rs4", tissue =  "Adipose_Subcutaneous", r2d_threshold = "99",
+                         token = Sys.getenv("LDLINK_TOKEN")
+                         )
+                )
+  expect_error(LDexpress(snps = "rs4", tissue =  "Adipose_Subcutaneous", p_threshold = "99",
+                         token = Sys.getenv("LDLINK_TOKEN")
+                         )
+                )
+  expect_error(LDexpress(snps = "rs4", tissue =  "Adipose_Subcutaneous", win_size = "9999999",
+                         token = Sys.getenv("LDLINK_TOKEN")
+                         )
+                )
+})
+
