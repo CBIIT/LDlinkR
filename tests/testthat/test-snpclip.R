43dd06429324a4207e1239fd87718f806afbb013
Tim <timothy.a.myers@gmail.com>
2022-05-04 08:07:51 -0400

    add tests to testthat for feature genome_build

--- tests/testthat/test-snpclip.R
+++ tests/testthat/test-snpclip.R
@@ -5,6 +5,22 @@ test_that("snpclip throws an error w/ bad rsID", {
   expect_error(SNPclip(c("rs3", "rs4", "148890987"), "YRI", 0.1, "0.01", token = Sys.getenv("LDLINK_TOKEN")))
 })
 
+test_that("snpclip throws an error w/ invalid genome_build args", {
+  skip_on_cran()
+  expect_error(snpclip(snps = c("rs3", "rs4"),
+                       pop = "YRI",
+                       r2_threshold = "0.1",
+                       maf_threshold = "0.01",
+                       token = "28da99809470",
+                       genome_build = "grch999"))
+  expect_error(snpclip(snps = c("rs3", "rs4"),
+                       pop = "YRI",
+                       r2_threshold = "0.1",
+                       maf_threshold = "0.01",
+                       token = "28da99809470",
+                       genome_build = c("grch38", "grch38_high_coverage")))
+})
+
 test_that("snpclip throws an error", {
   skip_on_cran()
   expect_named(SNPclip(c("rs3", "rs4", "rs148890987"), "YRI", 0.1, "0.01", token = Sys.getenv("LDLINK_TOKEN")))
