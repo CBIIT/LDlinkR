65deb9ab955d78fbf10e8f0dbeb3eb8bc3755973
Tim <timothy.a.myers@gmail.com>
2022-05-05 13:12:49 -0400

    update chip list

--- R/SNPchip.R
+++ R/SNPchip.R
@@ -65,7 +65,7 @@ array_abbrev <- function(array_name) {
               "I_Psyc-24v1.1", "A_10X",	"A_250N",	"A_250S",	"A_50H",	"A_50X",	"A_AFR",
               "A_ASI", "A_CHB2",	"A_DMETplus",	"A_EAS",	"A_EUR",	"A_Exome1A",
               "A_Exome319", "A_Hu",	"A_Hu-CHB",	"A_LAT",	"A_Onco",	"A_OncoCNV",
-              "A_SNP5.0", "A_SNP6.0", "A_UKBA")
+              "A_PMRA", "A_SNP5.0", "A_SNP6.0", "A_UKBA")
 
   return(abbrev[match(trimws(array_name),arrays)])
 }
@@ -158,16 +158,17 @@ LD_config <- list(snpchip_url_base="https://ldlink.nci.nih.gov/LDlinkRest/snpchi
                                "I_CNV370-Q","I_CVD","I_CardioMetab","I_Core-12","I_CoreE-12v1",
                                "I_CoreE-12v1.1","I_CoreE-24v1","I_CoreE-24v1.1","I_Cyto-12v2",
                                "I_Cyto-12v2.1","I_Cyto-12v2.1f","I_Cyto850","I_Exome-12",
-                               "I_Exon510S","I_Immuno-24v1","I_Immuno-24v2","I_Linkage-12",
-                               "I_Linkage-24","I_ME-Global-8","I_NS-12","I_O1-Q","I_O1S-8",
-                               "I_O2.5-4","I_O2.5-8","I_O2.5E-8v1","I_O2.5E-8v1.1","I_O2.5E-8v1.2",
-                               "I_O2.5S-8","I_O5-4","I_O5E-4","I_OE-12","I_OE-12f","I_OE-24",
-                               "I_OEE-8v1","I_OEE-8v1.1","I_OEE-8v1.2","I_OEE-8v1.3","I_OZH-8v1",
-                               "I_OZH-8v1.1","I_OZH-8v1.2","I_OncoArray","I_Psyc-24v1","I_Psyc-24v1.1",
-                               "A_10X","A_250N","A_250S","A_50H","A_50X","A_AFR","A_ASI","A_CHB2",
-                               "A_DMETplus","A_EAS","A_EUR","A_Exome1A","A_Exome319","A_Hu","A_Hu-CHB",
-                               "A_LAT","A_Onco","A_OncoCNV", "A_PMRA", "A_SNP5.0","A_SNP6.0",
-                               "A_UKBA", "ALL_Illumina", "ALL_Affy", "ALL"),
+                               "I_Exon510S","I_GSA-v1", "I_GSA-v2","I_Immuno-24v1","I_Immuno-24v2",
+                               "I_Linkage-12", "I_Linkage-24","I_ME-Global-8","I_MEGA","I_NS-12",
+                               "I_O1-Q","I_O1S-8","I_O2.5-4","I_O2.5-8","I_O2.5E-8v1",
+                               "I_O2.5E-8v1.1","I_O2.5E-8v1.2","I_O2.5S-8","I_O5-4","I_O5E-4",
+                               "I_OE-12","I_OE-12f","I_OE-24","I_OEE-8v1","I_OEE-8v1.1",
+                               "I_OEE-8v1.2","I_OEE-8v1.3","I_OZH-8v1","I_OZH-8v1.1",
+                               "I_OZH-8v1.2","I_OncoArray","I_Psyc-24v1","I_Psyc-24v1.1",
+                               "A_10X","A_250N","A_250S","A_50H","A_50X","A_AFR","A_ASI",
+                               "A_CHB2","A_DMETplus","A_EAS","A_EUR","A_Exome1A","A_Exome319",
+                               "A_Hu","A_Hu-CHB","A_LAT","A_Onco","A_OncoCNV","A_PMRA",
+                               "A_SNP5.0","A_SNP6.0","A_UKBA","ALL_Illumina","ALL_Affy","ALL"),
                    avail_genome_build = c("grch37", "grch38", "grch38_high_coverage")
                              )
 
