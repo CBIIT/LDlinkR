c2ec5439064670ec552ff0055f4bfc5074105af0
Tim <timothy.a.myers@gmail.com>
2024-03-04 12:01:55 -0500

    update tests for ldmatrix

--- tests/testthat/test-ldmatrix.R
+++ tests/testthat/test-ldmatrix.R
@@ -3,7 +3,6 @@ context("test-ldmatrix")
 test_that("ldmatrix throws an error", {
   skip_on_cran()
   skip_on_ci()
-  expect_error(LDmatrix("rs3", "YRI", "r2", token = Sys.getenv("LDLINK_TOKEN")))
   expect_error(LDmatrix(snps = c("rs4853519", "rs7598883", "Rs4853736"),
                         pop = c("YRI","CEU"),
                         r2d = "r2",
@@ -22,4 +21,29 @@ test_that("ldmatrix works", {
   skip_on_cran()
   skip_on_ci()
   expect_named(LDmatrix(c("rs3", "rs4", "rs148890987"), "YRI", "r2", token = Sys.getenv("LDLINK_TOKEN")))
+})
+
+
+test_that("ldmatrix throws an error when list of variants is greater than 2500", {
+  skip_on_cran()
+  skip_on_ci()
+  # generate list of variants that exceeds 2500
+  rs_variants <- paste0("rs", 1:2501)
+  expect_error(LDmatrix(snps = rs_variants,
+                        pop = "YRI",
+                        r2d = "r2",
+                        token = Sys.getenv("LDLINK_TOKEN")
+                       )
+               )
+})
+
+test_that("ldmatrix throws an error when list of variants is less than 2", {
+  skip_on_cran()
+  skip_on_ci()
+  expect_error(LDmatrix(snps = "rs3",
+                        pop = "YRI",
+                        r2d = "r2",
+                        token = Sys.getenv("LDLINK_TOKEN")
+                       )
+              )
 })
